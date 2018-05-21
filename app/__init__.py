0ae88692756c3571fe443a16ed44dd7aa5edc366
Emre Cetin <emrec.tin@gmail.com>
2018-05-21 15:38:50 +0300

    disable admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 from app.api.user import bp as user_bp
 from app.api.question import bp as question_bp
 from flask_admin import Admin
-from app.admin import admin_session, ModelView, User, Question
+# from app.admin import admin_session, ModelView, User, Question
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-admin = Admin(app, name='Admin-Panel', template_mode='bootstrap3')
-admin.add_view(ModelView(User, admin_session))
-admin.add_view(ModelView(Question, admin_session))
+# admin = Admin(app, name='Admin-Panel', template_mode='bootstrap3')
+# admin.add_view(ModelView(User, admin_session))
+# admin.add_view(ModelView(Question, admin_session))
 
 app.register_blueprint(user_bp)
 app.register_blueprint(question_bp)
